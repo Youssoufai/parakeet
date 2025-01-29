50b41cb248c44799f0a69971ba909148238ec67f
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-29 21:21:34 +0100

    Second Commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import Layout from "../Layout/Layout"
+
+const Home = ({ name }) => {
+    return (
+        <div>Home {name} </div>
+    )
+}
+
+/* Home.layout = page => <Layout children={page} /> */
+export default Home;
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,13 @@
 import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
+import Layout from './Layout/Layout';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
+    let page = pages[`./Pages/${name}.jsx`]
+    page.default.layout = page.default.layout || ((page) => <Layout children={page} />)
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
